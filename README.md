db32d4c2f2d5bc0a3ca16b4b6150aa359854cda2
Adrian Bernabeu <mentan017@gmail.com>
2024-06-25 03:18:41 +0200

    Added a init script and some basic instructions in a README file

---
+++ README.md
@@ -0,0 +1,17 @@
+## Requirements
+ - Node JS
+ - npm
+ - openssl (only for initializing the project)
+ - MongoDB
+
+## Initializing the project
+To initialize the project, you have to execute the _init\_script.sh_ file. Paste these lines in your terminal to get you started.
+```
+chmod +x ./init_script.sh
+./init_script.sh
+```
+## Start the project (with nodemon)
+To start the project, use the following npm command
+```
+npm run App_Start
+```
---
+++ init_script.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo "[INFO] Initializing Springfest Apps V2 (SF_V2)"
+
+echo "[INFO] Installing npm dependencies"
+npm i
+
+echo "[INFO] Creating the SSL certificate"
+mkdir cert
+cd cert
+openssl req -newkey rsa:4096 -x509 -sha256 -days 1000 -nodes -subj '/C=BE/ST=Brussels/L=Auderghem/O=Local_App' -out server.crt -keyout server.key
+cd ../
+
+echo "[INFO] Creating a resources folder"
+mkdir resources
+
+echo "[INFO] Creating default environment variables"
+touch .env
+echo "PROJECT_NAME=\"Springfest_Apps_V2\"" >> .env
+echo "PORT=\"2024\"" >> .env
+
+echo "[INFO] Done!"
